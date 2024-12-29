4aeb91f819be1a06e1b2a23450f2e7158c2962ae
TsukiSama9292 <a0985821880@gmail.com>
2024-12-29 18:08:45 +0800

    取消所有回傳的錯誤判斷

--- src/App.vue
+++ src/App.vue
@@ -156,21 +156,21 @@ export default {
                 const parsedData = JSON.parse(jsonData);
                 
                 console.log(parsedData.data.answer);  // 用來檢查返回的內容
-                // if (!parsedData.data.answer || parsedData.data.answer.length === 0) {
-                //   this.messages.push({ content: '沒有返回有效的資料，請檢查問題內容。', isUser: false });
+                // // if (!parsedData.data.answer || parsedData.data.answer.length === 0) {
+                // //   this.messages.push({ content: '沒有返回有效的資料，請檢查問題內容。', isUser: false });
+                // //   done = true;
+                // //   break;
+                // // }
+                // if (parsedData.data.answer.slice(0, 9) == "**ERROR**") {
+                //   this.messages.push({ content: '伺服器返回錯誤，請稍後再試。', isUser: false });
                 //   done = true;
                 //   break;
                 // }
-                if (parsedData.data.answer.slice(0, 9) == "**ERROR**") {
-                  this.messages.push({ content: '伺服器返回錯誤，請稍後再試。', isUser: false });
-                  done = true;
-                  break;
-                }
-                if (parsedData.data.answer.length >= 5000) {
-                  this.messages.push({ content: '回答過長，請換一句話重新傳送!', isUser: false });
-                  done = true;
-                  break;
-                }
+                // if (parsedData.data.answer.length >= 5000) {
+                //   this.messages.push({ content: '回答過長，請換一句話重新傳送!', isUser: false });
+                //   done = true;
+                //   break;
+                // }
                 if((parsedData.data.answer != undefined && parsedData.data.answer!='')&& first_push==false){
                   this.messages.push({ content: parsedData.data.answer, isUser: false });
                   first_push=true;
